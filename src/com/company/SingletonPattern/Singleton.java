327ecf43ab4340018402a5a71e4bba627f355fcf
Wojciech Dudek <wojciech.dudek@amsterdamstandard.com>
2020-05-12 13:09:09 +0200

    Implement Bill Pugh singleton

--- src/com/company/SingletonPattern/Singleton.java
+++ src/com/company/SingletonPattern/Singleton.java
@@ -1,23 +1,19 @@
 package com.company.SingletonPattern;
 
 public class Singleton {
-    private static Singleton uniqueInstance = null;
     private int data = 0;
 
     private Singleton() {}
 
+    private static class SingletonHelper {
+        // Nested class is referenced after getInstance() is called
+        private static final Singleton uniqueInstance = new Singleton();
+    }
+
     // by adding the synchronized keyword we force every thread
     // to wait its turn before it can access the method
     public static Singleton getInstance() {
-        if (uniqueInstance == null) {
-            synchronized (Singleton.class) {
-                if (uniqueInstance == null) {
-                    uniqueInstance = new Singleton();
-                }
-            }
-        }
-
-        return uniqueInstance;
+        return SingletonHelper.uniqueInstance;
     }
 
     public int getData() {
